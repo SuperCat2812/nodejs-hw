f282a72dbf285b4aa5e742d6b120e19cff28bcf7
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-07-22 15:55:54 +0200

    refactor: reorganize server setup and fix route parameter naming

--- src/server.js
+++ src/server.js
@@ -1,20 +1,33 @@
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
 import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+      },
+    },
+  }),
+);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
 });
 
-app.get('/notes/:userId', (req, res) => {
-  const id_params = req.params.userId;
-  res.status(200).json({ message: `Retrieved note with ID: ${id_params}` });
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
 app.get('/test-error', (req, res) => {
@@ -30,4 +43,8 @@ app.use((err, req, res, next) => {
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
