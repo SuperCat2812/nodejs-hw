6469b913b96ef9d995d63c60779892f3ca35a808
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-08-08 18:29:31 +0200

    fix: allow empty string for search parameter in getAllNotesSchema

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -9,7 +9,7 @@ export const getAllNotesSchema = {
     page: Joi.number().min(1).default(1),
     perPage: Joi.number().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
-    search: Joi.string(),
+    search: Joi.string().allow(''),
   }),
 };
 
