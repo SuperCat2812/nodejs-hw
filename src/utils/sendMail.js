f50fb0377c6152a59262133455dafa0808455cb9
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-08-23 20:48:27 +0200

    fix: remove TLS configuration and SMTP transporter verification from sendMail utility

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -9,17 +9,8 @@ const transporter = nodemailer.createTransport({
   port: Number(SMTP_PORT),
   secure: false,
   auth: { user: SMTP_USER, pass: SMTP_PASSWORD },
-  tls: {
-    rejectUnauthorized: false,
-  },
 });
-transporter.verify((error) => {
-  if (error) {
-    console.error('SMTP VERIFY ERROR:', error);
-  } else {
-    console.log('SMTP server is ready');
-  }
-});
+
 export const sendEmail = async ({ to, subject, html }) => {
   return transporter.sendMail({
     from: SMTP_FROM,
